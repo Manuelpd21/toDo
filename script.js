427553ad9898dcc73dba2b5ce25373db6e75ed49
Manuelpd21 <manuelpico121314@gmail.com>
2024-08-15 20:40:40 -0500

    jojo

--- script.js
+++ script.js
@@ -13,30 +13,62 @@ function addTodo() {
     }
 
     const li = document.createElement('li');
-    li.textContent = todoText;
+
+    // Create checkbox for marking as completed
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.className = 'todo-checkbox';
+
+    // Add functionality to toggle completed state
+    checkbox.onchange = function() {
+        if (checkbox.checked) {
+            li.classList.add('completed');
+        } else {
+            li.classList.remove('completed');
+        }
+    };
+
+    li.appendChild(checkbox);
+
+    // The rest of your code...
+    const textNode = document.createTextNode(todoText);
+    li.appendChild(textNode);
 
     const editButton = document.createElement('button');
     editButton.textContent = 'Edit';
     editButton.className = 'edit';
 
+    // Add edit button functionality
+    editButton.onclick = function() {
+        const newTodoText = prompt('Enter new todo text');
+        if (newTodoText) {
+            textNode.textContent = newTodoText;
+        }
+    };
+
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'delete';
 
-    // Create the button container and append buttons
+    // Add delete button functionality
+    deleteButton.onclick = function() {
+        const isConfirmed = confirm('Are you sure you want to delete this item from the list?');
+        if (isConfirmed) {
+            todoList.removeChild(li);
+        }
+    };
+
     const buttonContainer = document.createElement('div');
     buttonContainer.className = 'button-container';
     buttonContainer.appendChild(editButton);
     buttonContainer.appendChild(deleteButton);
 
-    // Append the button container to the li
     li.appendChild(buttonContainer);
-
-    // Append the li to the todo list
     todoList.appendChild(li);
 
     newTodoInput.value = '';
 }
+
 
 
 // Add event listener to the "Add Todo" button
